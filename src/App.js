49aef6f3373f48a176f2d69da341d7bfe4251106
rehaan redkar <56068173+rehaanredkar15@users.noreply.github.com>
2021-11-10 10:53:20 +0530

    fix - Including Home Screen page component in App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-
 import './App.css';
+import Home from './pages/home/Home.js';
+
 
 function App() {
   return (
     <div className="App">
-       <h1>
-       App
-       
-       </h1>
+       <Home/>
     </div>
   );
 }
