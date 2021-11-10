216cb012d6a41195bcfdcc84ec5562ddb35851f2
rehaan redkar <56068173+rehaanredkar15@users.noreply.github.com>
2021-11-10 10:48:38 +0530

    feat - Including the Topbar component in the Home page
    Also adding the home.css

---
+++ src/pages/home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Home.css';
+import TopBar from '../../components/Topbar/Topbar.jsx';
+
+const Home = () => {
+    return (
+        <div>
+            <TopBar/>
+        </div>
+    )
+}
+
+export default Home
