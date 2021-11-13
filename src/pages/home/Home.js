b1d51111bb9411bb8db23eb0f58ab05e80ce4d3a
rehaan redkar <56068173+rehaanredkar15@users.noreply.github.com>
2021-11-13 10:45:37 +0530

    Feat - Adding the Sections and other components in home page

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import './Home.css';
+import Feed from '../../components/Feed/Feed.jsx';
+import SideBar from '../../components/Sidebar/Sidebar.jsx';
+import RightBar from '../../components/Rightbar/RightBar.jsx';
 import TopBar from '../../components/Topbar/Topbar.jsx';
 
 const Home = () => {
     return (
-        <div>
+        <>
             <TopBar/>
-        </div>
+            <div className="homeContainer">
+            <SideBar/>
+             <Feed/>
+             <RightBar/>
+            </div>
+        </>
     )
 }
 
