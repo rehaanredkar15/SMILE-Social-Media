23600b66632a20694a62dcf8a67898ab1f16fb42
rehaan redkar <56068173+rehaanredkar15@users.noreply.github.com>
2021-11-17 17:50:41 +0530

    feat - Adding the Feed page to show the feed wall
    The Feed page will contain the post and share part

--- src/components/Feed/Feed.jsx
+++ src/components/Feed/Feed.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
 import './Feed.css';
+import Share from '../Share/Share.jsx';
+import Post from '../Post/Post.jsx';
 
 const Feed = () => {
     return (
         <div className = "FeedContainer">
-            Feed
+            <div className="FeedWrapper">
+              <div className="share">
+              <Share/>
+              </div>
+              <div className="PostsWall">
+              <div className="postCover">
+               <Post/>
+              </div>
+              <div className="postCover">
+               <Post/>
+              </div>
+              <div className="postCover">
+               <Post/>
+              </div>
+              <div className="postCover">
+               <Post/>
+              </div>
+              </div>
+            </div>
         </div>
     )
 }
