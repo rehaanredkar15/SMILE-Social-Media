dd944e4b9f27722d6af26002d70a0b4afcc5ec82
rehaan redkar <56068173+rehaanredkar15@users.noreply.github.com>
2021-11-17 18:06:04 +0530

    feat - Adding the right bar and the online Friends

--- src/components/Rightbar/RightBar.jsx
+++ src/components/Rightbar/RightBar.jsx
@@ -4,7 +4,54 @@ import './RightBar.css';
 const RightBar = () => {
     return (
         <div className="RightBarContainer">
-            RightBar
+            <div className="rightBarWrapper">
+            <div className="rightTop">
+              <div className="EventsContainer">
+               <img src="/Assets/events.png" alt="" className="eventImg" />
+               <span className="eventText"> 
+                  <b>Rehaan Redkar </b>and<b> 3 other friends </b>are celebrating an event
+               </span>
+              </div>
+              <img src="/Assets/event.png" alt="" className="rightBarAd" />
+              </div>
+
+
+              {/* Online Friends */}
+              <div className="friendsListWrapper">   
+                <div className="title">
+                <h4 className="rightBarTitle">Online Friends</h4>
+                </div>
+                <ul className="rightBarFriendList">
+                    <li className="rightBarFriend">
+                        <div className = "FriendDetails">
+                        <div className="rightBarProfileImgContainer">
+                        <img src="/Assets/friend.jpg" alt="" className="rightBarFriendImg" />
+                        <span className="rightBarOnline"></span>
+                        </div>
+                        <span className="rightBarFriendName"> Zayn Malik</span>
+                        </div>
+                     </li>
+                    <li className="rightBarFriend">
+                        <div className = "FriendDetails">
+                        <div className="rightBarProfileImgContainer">
+                        <img src="/Assets/friend.jpg" alt="" className="rightBarFriendImg" />
+                        <span className="rightBarOnline"></span>
+                        </div>
+                        <span className="rightBarFriendName"> Zayn Malik</span>
+                        </div>
+                     </li>
+                    <li className="rightBarFriend">
+                        <div className = "FriendDetails">
+                        <div className="rightBarProfileImgContainer">
+                        <img src="/Assets/friend.jpg" alt="" className="rightBarFriendImg" />
+                        <span className="rightBarOnline"></span>
+                        </div>
+                        <span className="rightBarFriendName"> Zayn Malik</span>
+                        </div>
+                     </li>
+                 </ul>
+              </div>
+            </div>
         </div>
     )
 }
