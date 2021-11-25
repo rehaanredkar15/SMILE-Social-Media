5f8c1644dd988702da2e5d50860a3f36994e78a6
rehaan redkar <56068173+rehaanredkar15@users.noreply.github.com>
2021-11-25 20:18:25 +0530

    feat - Introducing the router for routing the pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import './App.css';
 import Home from './pages/home/Home.js';
+import Profile from './pages/profile/Profile.js';
+import Login from './pages/login/Login.jsx';
+import Register from './pages/Register/Register.jsx';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import TopBar from './components/Topbar/Topbar.jsx';
 
 
 function App() {
   return (
     <>
-       <Home/>
+      <BrowserRouter>
+       <Routes>
+         <Route path="/" element={<Home />} />
+         <Route path="/signup" element={<Register />} />
+         <Route path="/login" element={<Login />} />
+         <Route path="/profile/:username" element={<Profile/>} />
+       </Routes>
+      </BrowserRouter>
     </>
   );
 }
