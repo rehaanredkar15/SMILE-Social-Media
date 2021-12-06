a236355636f945009eff6e5b71aef11cb9aab65e
rehaan redkar <56068173+rehaanredkar15@users.noreply.github.com>
2021-12-06 23:00:10 +0530

    feat - Creating the most Imporant Reducer
    In the Reducer the actions which is expected is performed that is data is being set and the initial state is being changed

---
+++ src/Context/AuthReducer.js
@@ -0,0 +1,34 @@
+const AuthReducer = ( state, action) => {
+
+    switch(action.type){
+
+        case "LOGIN_START":
+          return{
+              user:null,
+              isFetching:true,
+              error:false,
+          };
+          
+
+        case "LOGIN_SUCCESS":
+           
+           return {
+              
+              user:action.payload,
+              isFetching:false,
+              error:false,
+
+           };
+
+        case "LOGIN_FAILURE":
+
+           return {
+               
+               user:null,
+              isFetching:true,
+              error:action.payload,
+           };
+    }
+}
+
+export default AuthReducer;
