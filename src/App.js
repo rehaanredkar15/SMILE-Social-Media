aacf3afa896143a9d240d64e7e0a7d875042ee84
rehaan redkar <56068173+rehaanredkar15@users.noreply.github.com>
2021-12-07 00:38:17 +0530

    fix - Fixed the routing issue for proper routes according to the authentication

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import React,{useRef,useContext} from 'react'
 import Home from './pages/home/Home.js';
 import Profile from './pages/profile/Profile.js';
 import Login from './pages/login/Login.jsx';
@@ -6,19 +7,23 @@ import Register from './pages/Register/Register.jsx';
 import {
   BrowserRouter,
   Routes,
-  Route
+  Route,
+  Navigate
 } from "react-router-dom";
 import TopBar from './components/Topbar/Topbar.jsx';
-
+import { AuthContext } from './Context/AuthContext';
 
 function App() {
+
+  const {user} = useContext(AuthContext);
+
   return (
     <>
       <BrowserRouter>
        <Routes>
-         <Route path="/" element={<Home />} />
-         <Route path="/signup" element={<Register />} />
-         <Route path="/login" element={<Login />} />
+         <Route path="/" element={user ? <Home/> : <Navigate to="/register"/> } />
+         <Route path="/login" element={user ? <Navigate to="/"/> : <Login />   } />
+         <Route path="/register" element={user ? <Navigate to="/"/> : <Register />} />
          <Route path="/profile/:username" element={<Profile/>} />
        </Routes>
       </BrowserRouter>
