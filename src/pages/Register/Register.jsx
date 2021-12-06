3c7ca95fd8b33aa6c856369aceb48222f0ea948d
rehaan redkar <56068173+rehaanredkar15@users.noreply.github.com>
2021-12-07 00:37:21 +0530

    feat - Adding the features similar to login button i.e api call and input

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,8 +1,37 @@
 import React from 'react'
 import './Register.css';
+import {useRef,useContext} from 'react'
+import {  RegisterCall } from '../APICalls';
+import { AuthContext } from '../../Context/AuthContext';
+import { CircularProgress } from "@material-ui/core";
+import { Link } from "react-router-dom";
+
+export default  function Register ()  {
+
+    const email = useRef();
+   const password = useRef();
+   const username= useRef();
+   const confirmPassword = useRef();
+   const { user,isFetching,error,dispatch} = useContext(AuthContext);
+
+  const handleClick = (e) => {
+     
+     e.preventDefault();
+
+    //the second parameter is dispatch 
+     RegisterCall({ username:username.current.value,
+     email:email.current.value,
+     password:password.current.value},dispatch);
 
 
-export default  function Login ()  {
+     console.log({
+     username:username.current.value,
+     email:email.current.value,
+     password:password.current.value,
+     confirmPassword:confirmPassword.current.value});
+
+   }
+
     return (
         <div className="login">
           <div className="loginWrapper">
@@ -14,15 +43,34 @@ export default  function Login ()  {
             </div>
             <div className="loginRight">
           <div className="loginBox">
-            <input placeholder="Username" className="loginInput" />
-            <input placeholder="Email" className="loginInput" />
-            <input placeholder="Password" className="loginInput" />
-            <input placeholder="ConfirmPassword" className="loginInput" />
-            <button className="loginButton">Register</button>
-            <span className="loginForgot">Forgot Password?</span>
-            <button className="loginRegisterButton">
-               Sign In
+           <form onSubmit={handleClick} className="loginBox">
+            <input placeholder="Username" ref={username} className="loginInput" />
+            <input placeholder="Email" type = "email" ref={email} className="loginInput" />
+            <input placeholder="Password" type = "password" required minLength="6" ref={password} className="loginInput" />
+            <input placeholder="ConfirmPassword" type = "password" required minLength="6" ref={confirmPassword}  className="loginInput" />
+             
+            
+            <button className="loginButton" disabled={isFetching}>
+            {isFetching ? (
+                <CircularProgress color="primary" size="20px" />
+              ) : (
+                "Register"
+              )}
+            
             </button>
+            <span className="loginForgot">Forgot Password?</span>
+            <Link to="/login">
+            <button className="loginRegisterButton">
+
+               {isFetching ? (
+                <CircularProgress color="primary" size="20px" />
+              ) : (
+                "Sign In"
+              )}
+               
+            </button>
+            </Link>
+             </form>
           </div>
         </div>
           </div>  
