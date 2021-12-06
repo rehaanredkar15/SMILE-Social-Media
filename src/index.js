aac5474665e2f993f04ce9082b82af5bc62584d0
rehaan redkar <56068173+rehaanredkar15@users.noreply.github.com>
2021-12-06 23:08:07 +0530

    fix - Adding the Provider to the index file so that every component gets the context access

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import {
-//   BrowserRouter,
-//   Routes,
-//   Route
-// } from "react-router-dom";
 import './index.css';
 import App from './App';
+import { AuthContextProvider } from "./Context/AuthContext";
 
 
-ReactDOM.render(<App />,document.getElementById('root')
+ReactDOM.render(
+
+            <AuthContextProvider>
+              <App />
+            </AuthContextProvider>
+              
+              ,
+              
+     document.getElementById('root')
 );
 
